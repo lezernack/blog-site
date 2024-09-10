c90917a5aca7c8885269ebba8b80dfecc87d6144
lezernack <luks.ezck@gmail.com>
2024-09-10 15:16:30 -0600

    added some buttons

--- src/Routes/Route.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Route = () => {
-  function create() {}
-};
-
-export default Route;
--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -6,22 +6,29 @@ import Navbar from "./Navbar";
 const Section1 = () => {
   return (
     <div>
-      <div className="header">
-        <input className="title" type="text" placeholder="Section1" />
-      </div>
-      <div className="section">
-        <div>
-          <input
-            className="list"
-            name="section1"
-            type="text"
-            placeholder="Placeholder"
-          />
+      <form>
+        <div className="header">
+          <input className="title" type="text" placeholder="Section1" />
         </div>
-        <div className="dscrptDiv">
-          <input className="dscrpt" type="text" placeholder="Description" />
+        <div className="section">
+          <form>
+            <input
+              className="list"
+              name="section1"
+              type="text"
+              placeholder="Placeholder"
+            />
+          </form>
+          <div className="dscrptDiv">
+            <input className="dscrpt" type="text" placeholder="Description" />
+          </div>
         </div>
-      </div>
+        <div className="btns">
+          <button className="Cbtn">Create</button>
+          <button className="Ebtn">Edit</button>
+          <button className="Dbtn">Delete</button>
+        </div>
+      </form>
     </div>
   );
 };
