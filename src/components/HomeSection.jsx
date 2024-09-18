1c73cd377fd12594cbcf6d414dc8fb6298559942
lezernack <luks.ezck@gmail.com>
2024-09-18 16:54:39 -0600

    fixed the navbar position wise

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "../css/global.css";
 import "../css/homeSection.css";
+import background from "../images/background1.jpg";
 import PictureSection from "./PictureSection";
 import Footer from "./Footer";
 
 const HomeSection = () => {
   return (
-    <>
+    <div className="home">
       <div className="Welcome">
-        <h1> Welcome </h1>
+        <img
+          src={background}
+          className="welcomeImg"
+          width="550px"
+          height="100px"
+        />
       </div>
 
       <div className="About">
@@ -21,7 +27,7 @@ const HomeSection = () => {
         <PictureSection />
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
