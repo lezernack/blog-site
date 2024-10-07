b0d60d8f3eb06346b712b394640332da563fb6ad
lezernack <luks.ezck@gmail.com>
2024-10-07 15:06:15 -0600

    got the blog done

--- src/components/BlogSection.jsx
+++ src/components/BlogSection.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "../css/global.css";
 import "../css/blogSection.css";
 import CommentSection from "./CommentSection";
+import dog from "../images/dog.jpg";
+import cat from "../images/cat.jpg";
 
 const BlogSection = () => {
   return (
@@ -14,15 +16,59 @@ const BlogSection = () => {
         <div className="dscrptDiv">
           <p className="dscrpt">
             Now really it comes down to the individual person and the living
-            space. First Cats;
+            space. So here&apos;s a really basic list of Pros and Cons for each
+            pet.
           </p>
           <br />
-          <ul>
-            <li>Pros</li>
-          </ul>
-          <ul>
-            <li>Cons</li>
-          </ul>
+          <div className="dogCat">
+            <img src={cat} height="250px" width="250px" />
+            <img src={dog} height="250px" width="250px" />
+          </div>
+          <br />
+          <div className="vs">
+            <div className="catList">
+              <h3 className="h3">Cats</h3>
+              <ul>
+                <li className="Item">Pros</li>
+                <li>They don&apos;t require too much attention:</li>
+                <br />
+                <li>They are independent creatures:</li>
+                <br />
+                <li>They&apos;re tiny, so don&apos;t take up much space:</li>
+              </ul>
+              <ul>
+                <li className="Item">Cons</li>
+                <li>They will break cups and fragile things:</li>
+                <br />
+                <li>Have to clean out litter box:</li>
+                <br />
+                <li>Won&apos;t follow commands:</li>
+              </ul>
+            </div>
+            <div className="dogList">
+              <h3 className="h3">Dogs</h3>
+              <ul>
+                <li className="Item">Pros</li>
+                <li>Easy to teach tricks:</li>
+                <br />
+                <li>Love attention:</li>
+                <br />
+                <li>Will guard you and your&apos; home:</li>
+                <br />
+              </ul>
+              <ul>
+                <li className="Item">Cons</li>
+                <li>Hate being alone:</li>
+                <br />
+                <li>Some are really loud:</li>
+                <br />
+                <li>
+                  Can chew on shoes or anything else that they&apos;re not
+                  suppose to:
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
 
--- src/components/pictureSection.jsx
+++ src/components/pictureSection.jsx
@@ -3,6 +3,8 @@ import "../css/global.css";
 import "../css/pictureSection.css";
 import book from "../images/book.png";
 import controller from "../images/controller.png";
+import dog from "../images/dog.jpg";
+import cat from "../images/cat.jpg";
 
 const pictureSection = () => {
   return (
@@ -21,6 +23,28 @@ const pictureSection = () => {
 
             <img
               src={controller}
+              className="card1-back"
+              height="200px"
+              width="200px"
+            />
+          </div>
+        </div>
+      </div>
+
+      <h1>Blog</h1>
+      <caption>These pictures are used in the Blog section</caption>
+      <div className="cards">
+        <div className="card1">
+          <div className="card1-inner">
+            <img
+              src={dog}
+              className="card1-front"
+              height="200px"
+              width="200px"
+            />
+
+            <img
+              src={cat}
               className="card1-back"
               height="200px"
               width="200px"
